8fe002c99d7e8709dd40f17d1fa734f93b22b135
sankalpbarriar <114381745+sankalpbarriar@users.noreply.github.com>
2024-05-11 10:09:39 +0530

    inital commit

---
+++ 01_basics/cahi.py
@@ -0,0 +1,3 @@
+from hello import fun
+
+fun(4)
---
+++ 01_basics/hello.py
@@ -0,0 +1,10 @@
+print("Hello World!")
+
+def fun(n):
+    print(n)
+
+fun("Sankalp Barriar")
+
+chai_one="lemon tea"
+chai_two="ginget tea"
+chai_three="masala tea"
